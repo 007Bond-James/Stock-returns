1eca5b86e9ad0e2e079ae2563d7d3fb85513070b
007Bond-James <negiaayush1810@gmail.com>
2025-09-27 12:25:14 +0530

    first commit

---
+++ Alexander the Great.py
@@ -0,0 +1,7 @@
+from colorama import Fore, Style
+statement=(Fore.GREEN + "Alexander the Great \nThe making of GOD\t--" + Style.RESET_ALL)
+times=int(input("Enter the number of times you want to print the statement: "))
+for f in range(times):
+    print(statement,end=Fore.BLUE +"\tdefeated by none\texcept patriarc of CHAND FAMILY\n" + Style.RESET_ALL)
+print(Fore.YELLOW + "End of the program" + Style.RESET_ALL)
+
---
+++ Stock_Returns.py
@@ -0,0 +1,35 @@
+'''   ---Stock Returns Calculator---   '''
+
+from colorama import Fore, Style                                                        #Importing colorma for colored terminal text
+def Stock_returns_calculator():
+    print(Fore.CYAN + Style.BRIGHT + "\n \t\t---Welcome to Stock Returns Calculator---" + Style.RESET_ALL)
+    try:
+    #Take input for buy price, sell price and no of shares
+        buy_price = float(input(Fore.YELLOW+ "Buy Price   : " + Style.RESET_ALL))
+        sell_price = float(input(Fore.YELLOW + "Sell Price  : " + Style.RESET_ALL))
+        shares=int(input(Fore.YELLOW +"Quantity    : "+Style.RESET_ALL))
+    
+        returns= sell_price - buy_price
+        total_returns= (returns*shares)                                                   #Calcuates total reurns based on number of shares
+        percentage_returns=((returns/buy_price)*100)                                    #Calculates percentage returns
+
+    #Adding layers to display profit in green, loss in red and brek-even in yellow.   
+    
+        def print_result(label, value):
+            if value>0:
+                print(Fore.GREEN + Style.BRIGHT + f"{label} {value}"+ Style.RESET_ALL)   #Displays Profit in Green
+            elif value<0:
+                print(Fore.RED + Style.BRIGHT + f"{label} {value}"+ Style.RESET_ALL)     #Displays Loss in Red
+            else:
+                print(Fore.YELLOW + Style.BRIGHT + f"{label} {value}"+ Style.RESET_ALL)  #Displays Break-even in Yellow
+
+        print_result("Absolute P/L: ", total_returns)  
+        print_result("Return      : ", percentage_returns)
+        
+    #If user enters a non-numeric value, error message wil be displayed in red. 
+    
+    except ValueError:
+        print(Fore.RED +"Invalid input, please enter a numberic value." + Style.RESET_ALL)
+
+#Calling the Function to run the calculator
+Stock_returns_calculator()
